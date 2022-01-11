86a5783ce3efbcaff2a16c942c81cc0f848c2f25
Emiliano Mayer Viegas <emilianomayerviegas@gmail.com>
2022-01-10 21:25:01 -0300

    cv actualizacion

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,76 +1,58 @@
-import React from "react";
+import React from 'react';
 
-import OP from "../assets/img/OP.jpg";
-import mpmarkets from "../assets/img/mpmarkets.jpg";
-import kiboAR from "../assets/img/kiboAR.jpg";
-import Isquared from "../assets/img/ISQUARED.jpg";
-import wedding from "../assets/img/wedding.jpg";
-import ccr from "../assets/img/ccr.jpg";
+import OP from '../assets/img/OP.jpg';
+import kiboAR from '../assets/img/kiboAR.jpg';
+import Isquared from '../assets/img/ISQUARED.jpg';
+import wedding from '../assets/img/wedding.jpg';
+import ccr from '../assets/img/ccr.jpg';
 
-import "../css/Portfolio.css";
+import '../css/Portfolio.css';
 
 const Portfolio = () => {
   return (
     <React.Fragment>
-      <div className="block-portfolio">
-        <div className="title-porfolio">
+      <div className='block-portfolio'>
+        <div className='title-porfolio'>
           <h2>PORTFOLIO</h2>
         </div>
-        <div className="box-works">
-          <div className="work-container">
+        <div className='box-works'>
+          <div className='work-container'>
             <h3>OP AMOBLAMIENTOS</h3>
             <figure>
-              <a href="https://www.opamoblamientos.com/" target="blank">
-                <img src={OP} alt="" />
+              <a href='https://www.opamoblamientos.com/' target='blank'>
+                <img src={OP} alt='' />
               </a>
             </figure>
           </div>
-          <div className="work-container">
+          <div className='work-container'>
             <h3>JK WEDDING PLANNER</h3>
             <figure>
-              <a
-                href="https://pruebasfriendlymarketing.us/jkbouquet/"
-                target="blank"
-              >
-                <img src={wedding} alt="" />
+              <a href='https://pruebasfriendlymarketing.us/jkbouquet/' target='blank'>
+                <img src={wedding} alt='' />
               </a>
             </figure>
           </div>
-          <div className="work-container">
+          <div className='work-container'>
             <h3>CCR STEEL</h3>
             <figure>
-              <a
-                href="https://pruebasfriendlymarketing.us/ccrsteel/"
-                target="blank"
-              >
-                <img src={ccr} alt="" />
+              <a href='https://ccrsteel.com/' target='blank'>
+                <img src={ccr} alt='' />
               </a>
             </figure>
           </div>
-          <div className="work-container">
-            <h3>MP MARKETS</h3>
-            <figure>
-              <a
-                href="https://pruebasfriendlymarketing.us/mpmarkets/"
-                target="blank"
-              >
-                <img src={mpmarkets} alt="" />
-              </a>
-            </figure>
-          </div>
-          <div className="work-container">
+          <div className='work-container'>
             <h3>KIBOVISIÓN</h3>
             <figure>
-              <a href="https://www.kibovision.com.ar/" target="blank">
-                <img src={kiboAR} alt="" />
+              <a href='https://www.kibovision.com.ar/' target='blank'>
+                <img src={kiboAR} alt='' />
               </a>
             </figure>
           </div>
-          <div className="work-container">
+          <div className='work-container'>
             <h3>iSQUARED LATAM</h3>
             <figure>
-              <a href="https://www.isquared-la.com/" target="blank">
-                <img src={Isquared} alt="" />
+              <a href='https://www.isquared-la.com/' target='blank'>
+                <img src={Isquared} alt='' />
               </a>
             </figure>
           </div>
