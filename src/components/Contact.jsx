7f2cf11978b718d7cb807592bcbcdad7bc161e14
https://github.com/Emimayerk <emilianomayerviegas@gmail.com>
2024-01-04 15:28:02 -0300

    actualizacion botones de contacto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,25 +12,25 @@ const Contact = () => {
           <h2>CONTACTO</h2>
         </div>
         <div className='box-qr'>
-          <a href='mailto: emilianomayerviegas@gmail.com' target='blank_'>
+          <a class='button' href='mailto: emilianomayerviegas@gmail.com' target='blank_'>
             <div className='description-qr'>
               <h5>GMAIL</h5>
               <QRCode className='qr-img' value='mailto: emilianomayerviegas@gmail.com' size='85' bgColor='#272727' fgColor='#fff' />
             </div>
           </a>
-          <a href='tel:+543465405225' target='blank_'>
+          <a class='button' href='tel:+543465405225' target='blank_'>
             <div className='description-qr'>
               <h5> TELÉFONO </h5>
               <QRCode className='qr-img' value='tel:+543465405225' size='85' bgColor='#272727' fgColor='#fff' />
             </div>
           </a>
-          <a href='https://api.whatsapp.com/send?phone=+543465405225' target='blank_'>
+          <a class='button' href='https://api.whatsapp.com/send?phone=+543465405225' target='blank_'>
             <div className='description-qr'>
               <h5>WHATSAPP</h5>
               <QRCode className='qr-img' value='https://api.whatsapp.com/send?phone=+543465405225' size='85' bgColor='#272727' fgColor='#fff' />
             </div>
           </a>
-          <a href='https://www.linkedin.com/in/emiliano-mayer-viegas-03a0a41b2/' target='blank_'>
+          <a class='button' href='https://www.linkedin.com/in/emiliano-mayer-viegas-03a0a41b2/' target='blank_'>
             <div className='description-qr'>
               <h5>LINKEDIN</h5>
               <QRCode className='qr-img' value='https://www.linkedin.com/in/emiliano-mayer-viegas-03a0a41b2/' size='85' bgColor='#272727' fgColor='#fff' />
